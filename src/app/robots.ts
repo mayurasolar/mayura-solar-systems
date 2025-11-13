c3faa510a500c40d2c2a99b435e50d988ee96e2e
sandeepmshetty <sandeepmshetty@gmail.com>
2025-11-13 15:17:36 +0530

    refactor: Update robots rules format to use an array for better compatibility with Next.js

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,15 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/api/',
-    },
+    // Use an array of rule objects to be explicit and compatible with
+    // different consumers and the expected Next.js Robots format.
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+    ],
     sitemap: 'https://mayurasolar.xyz/sitemap.xml',
   };
 }
